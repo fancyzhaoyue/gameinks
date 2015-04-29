166873955fe5ca8e2ae7d2412c78ce2fe2cb506b
fancyzhaoyue <fancyzhaoyue@gmail.com>
2015-04-29 23:06:58 +0800

    加入angularjs

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,48 @@
 !function(){
-    angular.module('vx.index_controller', []).controller("index_ctrl", ["$rootScope", function($rootScope) {
-            $rootScope.global.title = '墨水游戏';
-    }]).controller('home_ctrl', ['$rootScope', '$scope', function($rootScope, $scope){
+    angular.module('vx.controller', []).controller('homeCtrl', ['$rootScope', '$scope', function($rootScope, $scope){
+        $rootScope.global.title = '墨水游戏';
+        $rootScope.global.hasGoback = false;
         $rootScope.getData('/data/games.json', function(data){
             $scope.games = data;
-            console.log(data);
-        })
+        });
+        $('.am-slider-manual').flexslider();
+    }]).controller('rankCtrl', ['$rootScope', '$scope', '$routeParams', function($rootScope, $scope, $routeParams){
+        console.log($routeParams.categoryId)
+        $scope.current_tab = 'hot';
+        $rootScope.global.title = '排行';
+        $rootScope.global.hasGoback = true;
+        $rootScope.getData('/data/games.json', function(data){
+            $scope.games = data;
+        });
+        $scope.changeTab = function(tabName){
+            $scope.current_tab = tabName;
+        }
+    }]).controller('cateCtrl', ['$rootScope', '$scope', function($rootScope, $scope){
+        $rootScope.global.title = '分类';
+        $rootScope.global.hasGoback = true;
+        $rootScope.getData('/data/category.json', function(data){
+            $scope.items = data;
+        });
     }]);
 }();
 
 var initOuterApp = function(){
     var vx = window.vx || {};
     vx.version = '0.0.1';
-    var modules = ['ngRoute', 'vx.index_controller'];
+    var modules = ['ngRoute', 'vx.controller'];
     vx.ng = angular.module('vx', modules).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
         $routeProvider.when('/',{
             templateUrl:'/views/home.html',
-            controller:'home_ctrl'
+            controller:'homeCtrl'
+        }).when('/rank',{
+            templateUrl:'/views/rank.html',
+            controller:'rankCtrl'
+        }).when('/rank/:categoryId',{
+            templateUrl:'/views/rank.html',
+            controller:'rankCtrl'
+        }).when('/category',{
+            templateUrl:'/views/category.html',
+            controller:'cateCtrl'
         }).otherwise({
             redirectTo: '/'
         });
@@ -26,7 +52,8 @@ var initOuterApp = function(){
             config: 'config',
             title: '',
             me: vx.me,
-            is_login: false,
+            isLogin: false,
+            hasGoback: false
         };
         $rootScope.getData = function(url, callaback, failback){
             $http.get(url).success(function(data ,status, headers, config){
